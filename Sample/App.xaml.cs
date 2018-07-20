5cd285967a37e3cb48d0a895f95e2e753879c202
hippieZhou <hippiezhou@outlook.com>
2018-07-20 12:04:14 +0800

    update

--- Sample/App.xaml.cs
+++ Sample/App.xaml.cs
@@ -15,9 +15,5 @@ namespace Sample
     /// </summary>
     public partial class App : Application
     {
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            base.OnStartup(e);
-        }
     }
 }
